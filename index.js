cdaab6fa3ba9b298d3af5dcee682d475c5a22457
JH220 <admin@jh220.de>
2022-08-24 21:35:35 +0200

    Added cors

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const logins = require('./logins.json');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const sequelize = new Sequelize(config.database, { logging: false });
