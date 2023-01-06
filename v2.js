d9310eadd16267953e96adf1661696b43a8b4466
JH220 <admin@jh220.de>
2023-01-06 17:31:21 +0100

    Fixed aggregate request

--- v2.js
+++ v2.js
@@ -21,44 +21,6 @@ app.get('/stations', async (req, res) => {
 	res.send(locations);
 });
 
-async function getData(entries, channels) {
-	
-	return data;
-}
-app.get('/stations/:id', async (req, res) => {
-	var id = parseInt(req.params.id);
-	if (!stations[id]) return res.status(400).send('The specified stationId does not exist!');
-
-	const startTimestamp = parseInt(req.query.start);
-	const endTimestamp = parseInt(req.query.end);
-	if (!(startTimestamp && endTimestamp))
-		return res.status(400).send('Please specify valid start and end timestamps!');
-	const startDate = new Date(startTimestamp * 1000);
-	const endDate = new Date(endTimestamp * 1000);
-	if (startDate.getTime() > endDate.getTime())
-		return res.status(400).send('The start date cannot be after the end date!');
-
-	const channels = (req.query.channels || 'temperature,humidity,air_pressure,air_particle_pm25,air_particle_pm10').split(',');
-	const entries = await Log.findAll({
-		where: {
-			stationId: id,
-			createdAt: {
-				[Op.between]: [startDate.toISOString(), endDate.toISOString()],
-			},
-		},
-	});
-
-	const data = {};
-	channels.forEach(channel => data[channel] = []);
-	entries.forEach(entry => {
-		const date = entry.createdAt;
-		channels.forEach(channel => {
-			if (entry[channel] != null)
-				data[channel].push({ x: date.toISOString(), y: entry[channel] });
-		});
-	});
-	res.send(data);
-});
 app.get('/stations/aggregate', async (req, res) => {
 	if (!req.query.ids) return res.status(400).send('Please specify station ids!');
 	const ids = req.query.ids.split(',');
@@ -93,6 +55,40 @@ app.get('/stations/aggregate', async (req, res) => {
 		channels.forEach(channel => {
 			if (entry[channel] != null)
 				data[entry.stationId][channel].push({ x: date.toISOString(), y: entry[channel] });
+		});
+	});
+	res.send(data);
+});
+app.get('/stations/:id', async (req, res) => {
+	var id = parseInt(req.params.id);
+	if (!stations[id]) return res.status(400).send('The specified stationId does not exist!');
+
+	const startTimestamp = parseInt(req.query.start);
+	const endTimestamp = parseInt(req.query.end);
+	if (!(startTimestamp && endTimestamp))
+		return res.status(400).send('Please specify valid start and end timestamps!');
+	const startDate = new Date(startTimestamp * 1000);
+	const endDate = new Date(endTimestamp * 1000);
+	if (startDate.getTime() > endDate.getTime())
+		return res.status(400).send('The start date cannot be after the end date!');
+
+	const channels = (req.query.channels || 'temperature,humidity,air_pressure,air_particle_pm25,air_particle_pm10').split(',');
+	const entries = await Log.findAll({
+		where: {
+			stationId: id,
+			createdAt: {
+				[Op.between]: [startDate.toISOString(), endDate.toISOString()],
+			},
+		},
+	});
+
+	const data = {};
+	channels.forEach(channel => data[channel] = []);
+	entries.forEach(entry => {
+		const date = entry.createdAt;
+		channels.forEach(channel => {
+			if (entry[channel] != null)
+				data[channel].push({ x: date.toISOString(), y: entry[channel] });
 		});
 	});
 	res.send(data);
