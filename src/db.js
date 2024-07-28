bf1666ba60e68dc33a62d165cd454ab7914ea91f
JH220 aka. Jan-Hendrik <admin@jh220.de>
2024-07-28 17:18:23 +0200

    Fix psql error for bigint air_pressure

--- src/db.js
+++ src/db.js
@@ -5,16 +5,16 @@ const Log = sequelize.define('logs', {
 	stationId: DataTypes.INTEGER,
 	temperature: DataTypes.FLOAT,
 	humidity: DataTypes.FLOAT,
-	air_pressure: DataTypes.INTEGER,
+	air_pressure: DataTypes.FLOAT,
 	air_particle_pm25: DataTypes.FLOAT,
 	air_particle_pm10: DataTypes.FLOAT,
 });
-Log.sync();
+Log.sync({ alter: true });
 
 const Error = sequelize.define('errors', {
 	stationId: DataTypes.INTEGER,
 	error: DataTypes.TEXT,
 });
-Error.sync();
+Error.sync({ alter: true });
 
 module.exports = { Log, Error };
