b5540ac7dcc4b6724fc0ec9839591ddab23d1541
JH220 <admin@jh220.de>
2025-05-13 11:10:46 +0200

    Remove filter

--- src/filter.js
+++ src/filter.js
@@ -1,7 +1,8 @@
 module.exports = {
 	async filter(entries) {
+		// eslint-disable-next-line no-unused-vars
 		entries = entries.filter(entry => {
-			if (entry.stationId == 1) entry.air_pressure = null;
+			// if (entry.stationId == 1) entry.air_pressure = null;
 			return true;
 		});
 		return entries;
